78fa374f53b22dc36886c30df504b372bb7fed1e
Minseo Kim(김민서) <neck3922@gmail.com>
2025-10-02 13:45:49 +0900

    삭제

--- server/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from .router_inspect import router as inspect_router
-
-app = FastAPI(title="Sentinel Inspector (mTLS)", version="1.1.0")
-app.include_router(inspect_router)
